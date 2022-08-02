428441d984ef005e31a389fc855624a3abe5f4ba
Prashanth <prashanth.cheepelli@yahoo.com>
2022-08-02 19:11:50 +0400

    data

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
-        Under Construction 🚧
-=======
         Under Construction ⚒️
->>>>>>> f8406d0b3fad7773ecab3bb44bb6741752ce1468
       </header>
     </div>
   );
